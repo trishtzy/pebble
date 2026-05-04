952cd23ef145825ec3e99c88593fa42d421435be
Tricia Tan <tricia.tzy@proton.me>
2026-05-04 11:11:36 +0800

    flake: bump pebble-tool to 5.0.34

--- flake.nix
+++ flake.nix
@@ -8,19 +8,20 @@
     devenv.url = "github:cachix/devenv";
   };
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
-
   outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
     let
       forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
     in
     {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
+      packages = forEachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devenv-up = self.devShells.${system}.default.config.procfileScript;
+          meow-o-clock = pkgs.callPackage ./meow-o-clock { };
+          perryverse = pkgs.callPackage ./watchface { };
+        });
 
       devShells = forEachSystem (system:
         let
@@ -37,7 +38,7 @@
                   version = "3.12";
                   venv.enable = true;
                   venv.requirements = ''
-                    pebble-tool==5.0.16
+                    pebble-tool==5.0.34
                   '';
                 };
 
@@ -47,7 +48,7 @@
                   ffmpeg
                 ];
 
-                pre-commit.hooks = {
+                git-hooks.hooks = {
                   clang-format = {
                     enable = true;
                     types_or = [ "c" "c++" ];
